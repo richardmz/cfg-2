82156ad46aea8eb9b30d18313033d1aca4543e83
Chen Li <mzlichen@163.com>
2021-06-24 09:37:55 +0800

    Add comment to separate types

--- src/main/java/fly/dream/cfg/ItemType.java
+++ src/main/java/fly/dream/cfg/ItemType.java
@@ -2,7 +2,12 @@ package fly.dream.cfg;
 
 public enum ItemType
 {
+    /**
+     * Only appears in parsing process. Eliminated in the parse result.
+     */
     PROPERTY,
+
+    
     STRING,
     MAP,
     LIST
