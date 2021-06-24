a0c019dd649a6860b6eacef3f2a493676f8668f3
Chen Li <mzlichen@163.com>
2021-06-24 09:38:24 +0800

    Add PropertyEliminationTest

---
+++ src/test/java/fly/dream/cfg/PropertyEliminationTest.java
@@ -0,0 +1,30 @@
+package fly.dream.cfg;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+public class PropertyEliminationTest
+{
+    public static void main(String[] args)
+    {
+        String filename = "config.cfg";
+        Path file = Paths.get(System.getProperty("user.dir") + File.separator + "test" + File.separator + filename);
+        try
+        {
+            Loader loader = new Loader(file);
+            Config config = loader.load();
+            Item mapItem = config.get("map");
+            System.out.println("Type of map: " + mapItem.getType());
+            Map<String, Item> map = mapItem.getMap();
+            Item property = map.get("property");
+            System.out.println("Type of property: " + property.getType());
+        }
+        catch (IOException | ConfigException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
